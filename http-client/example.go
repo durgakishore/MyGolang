5e2215f1c9810f0022fe3f244539c5634c0e8ae0
kishore <kishore53.yerubandi@gmail.com>
2020-10-15 20:22:39 +0530

    usage example updated

--- http-client/example.go
+++ http-client/example.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 var (
@@ -15,19 +16,30 @@ func getGithubClient() gohttp.HTTPClient {
 
 	client := gohttp.NewHTTPClient()
 
-	commonHeaders := make(http.Header)
-	commonHeaders.Set("Authorization", "Bearer ABC-123")
-	client.SetHeaders(commonHeaders)
+	//commonHeaders := make(http.Header)
+	//commonHeaders.Set("Authorization", "Bearer ABC-123")
+	//client.SetHeaders(commonHeaders)
+
+	client.SetMaxIdleConnections(20)
+	client.SetConnectionTimeout(2 * time.Second)
+	client.SetRequestTimeout(50 * time.Millisecond)
 
 	return client
 }
+
+//User struct
+type User struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
 func main() {
 
 	headers := make(http.Header)
 
-	//headers.Set("Authorization", "Bearer ABC-123")
+	headers.Set("Authorization", "Bearer ABC-123")
 
-	res, err := githubHTTPClient.Get("https://api.github.com", headers)
+	res, err := githubHTTPClient.Get("https://api.github.com" /*headers*/, nil)
 
 	if err != nil {
 		panic(err)
@@ -37,4 +49,9 @@ func main() {
 	bytes, _ := ioutil.ReadAll(res.Body)
 
 	fmt.Println(string(bytes))
+}
+
+func createUser(user User) {
+	//res, err := githubHTTPClient.Post("https://api.github.com", nil, user)
+
 }
